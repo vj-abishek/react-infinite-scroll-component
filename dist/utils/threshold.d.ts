23aea7c803a0d6d60b94bdef899b1ba1b8a16cbe
vj-abishek <abigosearch@gmail.com>
2021-02-20 11:02:20 +0000

    fix temp fix for mobile infinite scroll issue

---
+++ dist/utils/threshold.d.ts
@@ -0,0 +1,10 @@
+export declare const ThresholdUnits: {
+  Pixel: string;
+  Percent: string;
+};
+export declare function parseThreshold(
+  scrollThreshold: string | number
+): {
+  unit: string;
+  value: number;
+};
